fb1c4f50b01c040f21d2b4f75bb97c34e8079186
hudaaeda10 <hudaaeda10@gmail.com>
2020-07-03 07:04:47 +0700

    episode 25

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'SiteController@home');
+Route::get('/about', 'SiteController@about');
 
 Route::get('/login', 'AuthController@login')->name('login'); // memberi nama login untuk route
 Route::post('/postlogin', 'AuthController@postlogin');
